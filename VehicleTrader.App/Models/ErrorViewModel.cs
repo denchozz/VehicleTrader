dc6daad25de75f20304d8854e26ec813d3d9383b
slavov <denchozz@gmail.com>
2019-08-04 20:54:12 +0300

    VehicleTrader

---
+++ VehicleTrader.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VehicleTrader.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
