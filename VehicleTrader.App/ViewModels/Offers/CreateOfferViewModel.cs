f12a704228de9a808fba24c9e1dde1583df0b364
slavov <denchozz@gmail.com>
2019-08-30 17:09:28 +0300

    Login and Register forms

---
+++ VehicleTrader.App/ViewModels/Offers/CreateOfferViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleTrader.App.ViewModels.Offers
+{
+    public class CreateOfferViewModel
+    {
+
+    }
+}
